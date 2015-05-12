1b86379a871b2bd1743dd94eb8cd9c6d48c848e4
Ascii91 <milos2906@gmail.com>
2015-05-12 15:55:32 +0200

    Zapoceta izrada simulacionog procesa

---
+++ Yamb/src/com/etf/simulation/SimulationProgress.java
@@ -0,0 +1,26 @@
+package com.etf.simulation;
+
+import android.os.AsyncTask;
+
+public class SimulationProgress extends AsyncTask<Void, Void, Void>
+{
+
+    @Override
+    protected Void doInBackground(Void... params)
+    {
+     //TODO: ovde ce se pozivati sve ove ostale gluposti   
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        return null;
+    }
+
+}
